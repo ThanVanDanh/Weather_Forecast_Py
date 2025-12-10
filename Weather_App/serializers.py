60f13846c8114f88ceeaadfbd6691f9ada991c86
ThanVanDanhh <23130043@st.hcmuaf.edu.vn>
2025-12-10 15:01:32 +0700

    dataset

--- Weather_App/serializers.py
+++ Weather_App/serializers.py
@@ -1,38 +1,12 @@
-# weather/serializers.py
 from rest_framework import serializers
 from .models import AIForecast, WeatherAlert, Location, CurrentWeatherCache
 
 class LocationSerializer(serializers.ModelSerializer):
-    """Serializer cho thông tin cơ bản của Location (khi tìm kiếm)"""
     class Meta:
         model = Location
         fields = ('id', 'city_name', 'country_code', 'latitude', 'longitude')
 
-class AIForecastSerializer(serializers.ModelSerializer):
-    """
-    Serializer cho Bảng 4: Kết quả dự báo của AI (CẬP NHẬT)
-    """
-    class Meta:
-        model = AIForecast
-        fields = (
-            'forecast_date',
-            'predicted_temp_max',
-            'predicted_temp_min',
-            'predicted_precipitation_probability',
-            'predicted_weather_code'
-        )
-
-class WeatherAlertSerializer(serializers.ModelSerializer):
-    """Serializer cho Bảng 5: Cảnh báo thời tiết"""
-    class Meta:
-        model = WeatherAlert
-        fields = ('event_name', 'description', 'start_time', 'end_time')
-
-# --- THÊM SERIALIZER MỚI CHO "THÀNH PHỐ NỔI BẬT" ---
 class FeaturedCitySerializer(serializers.ModelSerializer):
-    """
-    Serializer cho các thành phố nổi bật (Bảng 1 + Bảng 2)
-    """
     current_weather = serializers.JSONField(
         source='current_weather_cache.data',
         read_only=True
--- Weather_App/services.py
+++ Weather_App/services.py
@@ -1,4 +1,3 @@
-# weather/services.py
 import requests
 from django.conf import settings
 
@@ -11,25 +10,18 @@ class MeteoAPIService:
         self.base_url = "https://api.open-meteo.com/v1/forecast"
 
     def fetch_current_weather(self):
-        """
-        Gọi API Meteo chi tiết hơn để lấy đủ dữ liệu cho HTML.
-        """
+    #get data hien tai
         params = {
             'latitude': self.lat,
             'longitude': self.lon,
             'timezone': 'Asia/Ho_Chi_Minh',
 
-            # 1. Dữ liệu hiện tại
-            # (Thêm weathercode, wind_speed)
             'current_weather': 'true',
 
-            # 2. Dữ liệu hôm nay (để lấy Thấp/Cao, UV)
             'daily': 'weathercode,temperature_2m_max,temperature_2m_min,uv_index_max,sunrise,sunset',
 
-            # 3. Dữ liệu giờ (để lấy Cảm giác như, Độ ẩm, Áp suất, Tầm nhìn)
             'hourly': 'temperature_2m,apparent_temperature,relativehumidity_2m,pressure_msl,visibility',
 
-            # Chỉ lấy 1 ngày (hôm nay)
             'forecast_days': 1
         }
 
@@ -41,7 +33,3 @@ class MeteoAPIService:
         except requests.RequestException as e:
             print(f"Lỗi gọi API Meteo: {e}")
             return None
-
-    def fetch_weather_alerts(self):
-        # (API Meteo không có cảnh báo cho VN, nên tạm thời trả về rỗng)
-        return []
